89d5e3b2490241248613684eba769ab8b71bf904
mandy <mandyhale10@gmail.com>
2025-04-08 13:17:27 +1200

    display ticket board on home with tickets

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TicketBoard from "./TicketBoard";
-import TicketDetails from "./TicketDetails";
+import ticketsData from "../tickets.json";
+// import TicketDetails from "./TicketDetails";
+
+
 
 function Main() {
   return (
-    <Router>
-      <TicketBoard />
-      <TicketDetails />
-    </Router>
+    <>
+      <Router>
+          <Routes>
+            <Route path="/" element={<TicketBoard tickets={ticketsData} />} />
+            {/* <TicketBoard /> */}
+            {/* <TicketDetails /> */}
+          </Routes>
+      </Router>
+    </>
   );
 }
 
