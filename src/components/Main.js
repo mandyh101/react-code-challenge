35cd474ec953379cc1e0d20e5622329f8ba64c48
mandy <mandyhale10@gmail.com>
2025-04-08 22:13:30 +1200

    tidy out removed import

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TicketBoard from "./TicketBoard";
-import ticketsData from "../tickets.json";
 import TicketDetails from "./TicketDetails";
 
 
