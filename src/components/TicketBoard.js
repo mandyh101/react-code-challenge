ab6fcca3fdc85412fdcf35646c717412f17f6eee
mandy <mandyhale10@gmail.com>
2025-04-08 21:40:16 +1200

    filter tickets by status and pass test - include steps in comments

--- src/components/TicketBoard.js
+++ src/components/TicketBoard.js
@@ -4,26 +4,31 @@ import { TicketContext } from "../context/TicketContext.js";
 
 const TicketBoard = () => {
 
-  const [status, setStatus] = React.useState("All");
+  const [statusFilter, setStatusFilter] = React.useState("All");
   
   const uniqueAgents = ["Agent1", "Agent2", "Agent3"];
 
   const statusOptions = ["All", "Open", "In Progress", "Resolved"];
   
+  //get the original data first
   const  {tickets} = useContext(TicketContext);
-
-  const sortedTickets = [...tickets].sort(
+  // then filter it
+  const filteredTickets = tickets.filter((ticket => statusFilter === "All" || ticket.status === statusFilter));
+  // then sort it to get the final tickets array for rendering
+  const sortedTickets = [...filteredTickets].sort(
     (a, b) => new Date(a.dueDate) - new Date(b.dueDate)
   );
 
-
+  const handleStatusChange = (EventTarget) => {
+    setStatusFilter(EventTarget.value);
+  }
 
   return (
     <div className="centered-container" data-testid="ticket-board">
       <h2 className="mb-10 mt-10">Ticket Board</h2>
       <div className="layout-row align-items-center">
         <label className="ml-20">Filter by Status: </label>
-        <select className="ml-10" data-testId="filter-status" value={"Open"}>
+        <select className="ml-10" data-testId="filter-status" value={statusFilter} onChange={(e) => handleStatusChange(e.target)}>
           {statusOptions.map((status, index) => (
             <option key={index} value={status}>
               {status}
