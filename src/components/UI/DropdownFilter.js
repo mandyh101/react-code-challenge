e87d53c80bc4f79e8713bdd4d5eb6def3a6d659d
mandy <mandyhale10@gmail.com>
2025-04-09 09:54:41 +1200

    fix: test id name mismatch

--- src/components/UI/DropdownFilter.js
+++ src/components/UI/DropdownFilter.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function DropdownFilter({options, selected, onChangeFn, filterName, datatestId}) {
+export default function DropdownFilter({options, selected, onChangeFn, filterName, dataTestId}) {
   return (
     <>
       <label className="ml-20">Filter by {filterName}: </label>
-        <select className="ml-10" data-testId={datatestId} value={selected} onChange={(e) => onChangeFn(e.target)}>
+        <select className="ml-10" data-testId={dataTestId} value={selected} onChange={(e) => onChangeFn(e.target)}>
           {options.map((option, index) => (
             <option key={index} value={option}>
               {option}
